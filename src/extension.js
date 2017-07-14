82c760ee6f22d7e2129338ae73a7378c053ca84b
llwslc <llwslc@gmail.com>
2017-07-14 17:20:06 +0800

    format my self

--- src/extension.js
+++ src/extension.js
@@ -5,36 +5,38 @@ var mJsBeautify = require('./mJsBeautify');
 
 exports.activate = function (context)
 {
-    var myConfig = {};
-    var getCfg = function ()
+  var myConfig = {};
+  var getCfg = function ()
+  {
+    myConfig = vscode.workspace.getConfiguration('beautify-my-code');
+  }
+
+  vscode.workspace.onWillSaveTextDocument(function (event)
+  {
+    if (!myConfig.function_setting.format_on_save) return;
+
+    var doc = event.document;
+    if (doc.languageId !== 'javascript' && doc.languageId !== 'json')
     {
-        myConfig = vscode.workspace.getConfiguration('beautify-my-code');
+      return;
     }
 
-    vscode.workspace.onWillSaveTextDocument(function (event)
-    {
-        var doc = event.document;
-        if (doc.languageId !== 'javascript' && doc.languageId !== 'json')
-        {
-            return;
-        }
+    var editorText = doc.getText();
+    var newText = mJsBeautify(editorText, myConfig, doc.languageId);
 
-        var editorText = doc.getText();
-        var newText = mJsBeautify(editorText, myConfig);
+    var fullRange = new vscode.Range(0, 0, Number.MAX_VALUE, Number.MAX_VALUE);
+    var we = new vscode.WorkspaceEdit();
 
-        var fullRange = new vscode.Range(0, 0, Number.MAX_VALUE, Number.MAX_VALUE);
-        var we = new vscode.WorkspaceEdit();
+    we.replace(doc.uri, fullRange, newText);
+    vscode.workspace.applyEdit(we);
+  });
 
-        we.replace(doc.uri, fullRange, newText);
-        vscode.workspace.applyEdit(we);
-    });
-
-    vscode.workspace.onDidChangeConfiguration(function ()
-    {
-        getCfg();
-    });
-
+  vscode.workspace.onDidChangeConfiguration(function ()
+  {
     getCfg();
+  });
+
+  getCfg();
 };
 
-exports.deactivate = function () { };
+exports.deactivate = function () {};
