fc9cb453ae476eb84c2b266451433625b21871d6
llwslc <llwslc@gmail.com>
2017-08-24 03:11:15 +0800

    perfect vue content

--- src/mVueBeautify.js
+++ src/mVueBeautify.js
@@ -10,6 +10,17 @@ var format = function (editorText, cfg)
   var beautifiedText = '';
   var parts = vueCompiler.parseComponent(editorText, {pad: 'space'});
 
+  if (!parts.template)
+  {
+    parts.template = {};
+    parts.template.content = '\n';
+  }
+  if (!parts.script)
+  {
+    parts.script = {};
+    parts.script.content = '';
+  }
+
   var html = mHtmlBeautify(`<template>${parts.template.content}</template>`, cfg);
   var js = mJsBeautify(parts.script.content.replace(/^ +$/mg, '  '), cfg, common.jsLanguageId);
   var css = [];
@@ -20,8 +31,9 @@ var format = function (editorText, cfg)
 
   beautifiedText = `${html}\n`;
   beautifiedText += `<script>\n`;
+  js = js.replace(/\n$/, '');
   beautifiedText += js;
-  if (!cfg.newline_setting.newline_at_end)
+  if (js.length)
   {
     beautifiedText += `\n`;
   }
